2d7a8c56b0965892d02df8b0882f74a4a64f40f7
godthyane <jose.daza01@uptc.edu.co>
2020-12-01 22:27:27 -0500

    Cors add

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var pokerRouter = require('./routes/pokertest');
 var mediaRouter = require('./routes/mediatest');
@@ -22,6 +24,8 @@ app.use('/pokertest', pokerRouter);
 app.use('/mediatest', mediaRouter);
 app.use('/cuadradosmedios', cMediosRouter);
 app.use('/generaterandom', generateRandomRouter);
+
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
