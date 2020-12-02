a30bc1dc66a571a7342bbf887c66e5196d24c7d6
godthyane <jose.daza01@uptc.edu.co>
2020-12-01 23:05:11 -0500

    Cors add x2

--- app.js
+++ app.js
@@ -27,4 +27,11 @@ app.use('/generaterandom', generateRandomRouter);
 
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 module.exports = app;
